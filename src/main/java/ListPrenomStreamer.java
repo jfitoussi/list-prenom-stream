a456c9168f8c0acea61ed3c069f2abdfce4784d5
KevinLeBrazidec <lebrazideck@hotmail.fr>
2017-03-29 15:05:42 +0200

    top32010

--- src/main/java/ListPrenomStreamer.java
+++ src/main/java/ListPrenomStreamer.java
@@ -1,9 +1,16 @@
+import static org.junit.Assert.assertThat;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.function.Predicate;
 
 import com.google.gson.Gson;
 
+import models.Fields;
 import models.ParisData;
 
 public class ListPrenomStreamer {
@@ -19,15 +26,17 @@ public class ListPrenomStreamer {
     public static void main(String[] args) throws IOException {
         ListPrenomStreamer listPrenomStreamer = new ListPrenomStreamer("liste_des_prenoms_2004_a_2012.json");
         System.out.println(listPrenomStreamer.getSize());
-        System.out.println(listPrenomStreamer.top3name2008());
+        System.out.println(listPrenomStreamer.top3name2010());
     }
 
     public int getSize() {
         return parisData.getRecords().size();
     }
 
-    public List<String> top3name2008() {
-        return null;
+    public List<String> top3name2010() {  	
+        Comparator<Fields> topOrder = (Fields c1, Fields c2) ->  Integer.compare(c1.getNombre(), c2.getNombre());
+        Stream<String> top3Prenoms2010Stream = this.parisData.getRecords().stream().map(fields -> fields.getFields()).sorted(topOrder.reversed()).filter(field -> field.getAnnee() == 2010).map(person -> person.getPrenoms()).limit(3);
+        return top3Prenoms2010Stream.collect(Collectors.toList());
     }
 
 }
--- src/test/java/ListPrenomStreamerTest.java
+++ src/test/java/ListPrenomStreamerTest.java
@@ -1,7 +1,11 @@
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+import java.util.List;
 
 import org.junit.Test;
+
 
 public class ListPrenomStreamerTest {
 
@@ -11,7 +15,16 @@ public class ListPrenomStreamerTest {
         ListPrenomStreamer listPrenomStreamer = new ListPrenomStreamer("liste_des_prenoms_2004_a_2012_short.json");
 
         // Then
-        assertThat(listPrenomStreamer.getSize(), is(10));
+        assertThat(listPrenomStreamer.getSize(), is(13));
+    }
+    
+    @Test
+    public void top_3_2010() throws Exception {
+        // Given
+        ListPrenomStreamer listPrenomStreamer = new ListPrenomStreamer("liste_des_prenoms_2004_a_2012_short.json");
+        List<String> top32010 = listPrenomStreamer.top3name2010();
+        // Then
+        assertThat(top32010, contains("Adam", "Alexandre", "Victor"));
     }
 
 }
