59991b7c4b7a5ca4ffad24b410d27e48f03eeb4f
abraham <brams945@gmail.com>
2017-03-29 15:57:14 +0200

    first commit

--- src/main/java/ListPrenomStreamer.java
+++ src/main/java/ListPrenomStreamer.java
@@ -1,10 +1,14 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.google.gson.Gson;
 
 import models.ParisData;
+import models.Records;
 
 public class ListPrenomStreamer {
 
@@ -27,7 +31,61 @@ public class ListPrenomStreamer {
     }
 
     public List<String> top3name2008() {
+
         return null;
     }
 
+    public List<Records> top3name2010() {
+
+        Comparator<Records> comparator = (Records r1, Records r2)-> {
+            return -((Integer)r1.getFields().getNombre()).compareTo(r2.getFields().getNombre());
+        };
+        Stream<Records> stream2010 = parisData.getRecords().stream().filter(records -> records.getFields().getAnnee() == 2010);
+
+        stream2010 = stream2010.sorted(comparator).limit(3);
+        return stream2010.collect(Collectors.toList());
+    }
+
+    public List<Records> top3GirlName2009() {
+        Comparator<Records> comparator = (Records r1, Records r2)-> {
+            return -((Integer)r1.getFields().getNombre()).compareTo(r2.getFields().getNombre());
+        };
+        Stream<Records> stream2009 = parisData.getRecords().stream().filter(records -> (records.getFields().getAnnee() == 2009 && records.getFields().getSexe().equals("F")));
+
+        stream2009 = stream2009.sorted(comparator).limit(3);
+        return stream2009.collect(Collectors.toList());
+    }
+
+    public List<Records> top3MenName2012() {
+        Comparator<Records> comparator = (Records r1, Records r2)-> {
+            return -((Integer)r1.getFields().getNombre()).compareTo(r2.getFields().getNombre());
+        };
+        Stream<Records> stream = parisData.getRecords().stream().filter(records -> (records.getFields().getAnnee() == 2012 && records.getFields().getSexe().equals("M")));
+
+        stream = stream.sorted(comparator).limit(3);
+        return stream.collect(Collectors.toList());
+
+    }
+
+    public List<Records> top5Name2009_2016() {
+        Comparator<Records> comparator = (Records r1, Records r2)-> {
+            return -((Integer)r1.getFields().getNombre()).compareTo(r2.getFields().getNombre());
+        };
+
+        Stream<Records> stream2012 = parisData.getRecords().stream().filter(records -> (records.getFields().getAnnee() >= 2009 && records.getFields().getAnnee() <= 2016));
+
+        stream2012 = stream2012.sorted(comparator).limit(5);
+        return stream2012.collect(Collectors.toList());
+    }
+
+    public List<Records> worst10Name2009_2016() {
+        Comparator<Records> comparator = (Records r1, Records r2)-> {
+            return ((Integer)r1.getFields().getNombre()).compareTo(r2.getFields().getNombre());
+        };
+
+        Stream<Records> stream = parisData.getRecords().stream().filter(records -> (records.getFields().getAnnee() >= 2009 && records.getFields().getAnnee() <= 2016));
+
+        stream = stream.sorted(comparator).limit(10);
+        return stream.collect(Collectors.toList());
+    }
 }
--- src/test/java/ListPrenomStreamerTest.java
+++ src/test/java/ListPrenomStreamerTest.java
@@ -1,8 +1,14 @@
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import models.Records;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import static org.hamcrest.Matchers.contains;
 public class ListPrenomStreamerTest {
 
     @Test
@@ -11,7 +17,67 @@ public class ListPrenomStreamerTest {
         ListPrenomStreamer listPrenomStreamer = new ListPrenomStreamer("liste_des_prenoms_2004_a_2012_short.json");
 
         // Then
-        assertThat(listPrenomStreamer.getSize(), is(10));
+        assertThat(listPrenomStreamer.getSize(), is(22));
+    }
+
+    @Test
+    public void top_3_best_2010(){
+
+        ListPrenomStreamer listPrenomStreamer = new ListPrenomStreamer("liste_des_prenoms_2004_a_2012_short.json");
+        List<Records> listTop3_2010 = listPrenomStreamer.top3name2010();
+
+        Function<Records, String> getNameFunction = records -> {return records.getFields().getPrenoms();};
+
+        List<String> nameList = listTop3_2010.stream().map(getNameFunction).collect(Collectors.toList());
+        assertThat(nameList, containsInAnyOrder("Abraham", "Harouna", "Ismael"));
+        //listPrenomStreamer.
+    }
+
+    @Test
+    public void top_3_best_girl_2009(){
+        ListPrenomStreamer listPrenomStreamer = new ListPrenomStreamer("liste_des_prenoms_2004_a_2012_short.json");
+        List<Records> listTop3Girl_2009 = listPrenomStreamer.top3GirlName2009();
+
+        Function<Records, String> getNameFunction = records -> {return records.getFields().getPrenoms();};
+
+        List<String> nameList = listTop3Girl_2009.stream().map(getNameFunction).collect(Collectors.toList());
+        assertThat(nameList, containsInAnyOrder( "Imane", "Islem", "Pauline"));
+
+    }
+
+    @Test
+    public void top_3_best_men_2012(){
+        ListPrenomStreamer listPrenomStreamer = new ListPrenomStreamer("liste_des_prenoms_2004_a_2012_short.json");
+        List<Records> listTop3Men_2012 = listPrenomStreamer.top3MenName2012();
+
+        Function<Records, String> getNameFunction = records -> {return records.getFields().getPrenoms();};
+
+        List<String> nameList = listTop3Men_2012.stream().map(getNameFunction).collect(Collectors.toList());
+        assertThat(nameList, containsInAnyOrder( "Kevin", "Ismo", "Flo"));
+
+    }
+
+    @Test
+    public void top_5_best_name_2009_2016(){
+        ListPrenomStreamer listPrenomStreamer = new ListPrenomStreamer("liste_des_prenoms_2004_a_2012_short.json");
+        List<Records> listTop5 = listPrenomStreamer.top5Name2009_2016();
+
+        Function<Records, String> getNameFunction = records -> {return records.getFields().getPrenoms();};
+
+        List<String> nameList = listTop5.stream().map(getNameFunction).collect(Collectors.toList());
+        assertThat(nameList, containsInAnyOrder( "Adam", "Alexandre", "Victor", "Liam", "Ethan"));
+    }
+
+    @Test
+    public void worst_10_best_name_2009_2016(){
+        ListPrenomStreamer listPrenomStreamer = new ListPrenomStreamer("liste_des_prenoms_2004_a_2012_short.json");
+        List<Records> listWorst10 = listPrenomStreamer.worst10Name2009_2016();
+
+        Function<Records, String> getNameFunction = records -> {return records.getFields().getPrenoms();};
+
+        List<String> nameList = listWorst10.stream().map(getNameFunction).collect(Collectors.toList());
+        assertThat(nameList, containsInAnyOrder( "Dom", "Juan", "Ibrahim", "Harouna", "Ismael", "Sophie", "Ismo", "Flo", "Kevin", "Abraham"));
+
     }
 
 }
